cb2645b23bb0cdf622db2727f810b2d11f014715
fabiano-manjaro <fabiano.analyst@gmail.com>
2024-01-19 18:53:02 -0300

    migration standalone

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,22 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './containers/courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
 import { CourseFormComponent } from './containers/course-form/course-form.component';
 
 @NgModule({
     imports: [
-        CommonModule,
-        CoursesRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        CoursesComponent,
-        CourseFormComponent,
-        CoursesListComponent
-    ]
+    CommonModule,
+    CoursesRoutingModule,
+    ReactiveFormsModule,
+    CoursesComponent,
+    CourseFormComponent,
+    CoursesListComponent
+]
 })
 export class CoursesModule { }
